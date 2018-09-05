8c35b439a40c3e2294cec0ba4d9fbadae8904a18
Porag <hasiburporag97@gmail.com>
2018-09-05 22:53:34 +0600

    Profile Page Office

--- app/src/main/java/com/example/porag/osmanyhallmanagement/StudentProfile.java
+++ app/src/main/java/com/example/porag/osmanyhallmanagement/StudentProfile.java
@@ -1,6 +1,8 @@
 package com.example.porag.osmanyhallmanagement;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -8,8 +10,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class StudentProfile extends AppCompatActivity {
@@ -20,6 +24,8 @@ public class StudentProfile extends AppCompatActivity {
     private SkipActivity session;
     ProgressDialog progressDialog;
 
+    CardView Gout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,7 @@ public class StudentProfile extends AppCompatActivity {
         progressDialog=new ProgressDialog(this);
         progressDialog.setMessage("Logging Out");
         dl = (DrawerLayout) findViewById(R.id.activity_student_profile);
+        Gout=findViewById(R.id.gout);
         t = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
         session=new SkipActivity(this);
         dl.addDrawerListener(t);
@@ -37,6 +44,35 @@ public class StudentProfile extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         nv = (NavigationView) findViewById(R.id.nv);
+
+        Gout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        if (!isFinishing()){
+                            new AlertDialog.Builder(StudentProfile.this)
+                                    .setTitle("Going Out")
+                                    .setMessage("Stay Outside at Night?")
+                                    .setCancelable(false)
+                                    .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+
+                                        }
+                                    }).setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            }).show();
+                        }
+                    }
+                });
+            }
+        });
 
         nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
