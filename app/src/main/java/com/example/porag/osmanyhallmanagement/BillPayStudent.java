df5a61487f74d02288071eaf9ad5792ed561596f
Porag <hasiburporag97@gmail.com>
2018-09-14 19:08:50 +0600

    Mess and payment design

---
+++ app/src/main/java/com/example/porag/osmanyhallmanagement/BillPayStudent.java
@@ -0,0 +1,26 @@
+package com.example.porag.osmanyhallmanagement;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class BillPayStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_pay_student);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
