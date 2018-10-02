23dd066076ba3eff4d9ff2c753dff2caabd6d071
Noor Asif <noormdashif@hmail.com>
2018-10-03 00:46:19 +0600

    StuffRegister

---
+++ app/src/main/java/com/example/porag/osmanyhallmanagement/StuffRegister.java
@@ -0,0 +1,13 @@
+package com.example.porag.osmanyhallmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StuffRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stuff_register);
+    }
+}
